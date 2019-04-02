82f8c257bd342a00509e6cd0c93ea816d0e484eb
JohnBetancourt <john.betancourt@javeriana.edu.co>
2019-04-02 08:22:51 -0500

    general updates and improvements

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ Overview
 ---
 This repository contains starting files for the Behavioral Cloning Project.
 
-In this project, you will use what you've learned about deep neural networks and convolutional neural networks to clone driving behavior. You will train, validate and test a model using Keras. The model will output a steering angle to an autonomous vehicle.
+In this project, I used what I've learned about deep neural networks and convolutional neural networks to clone driving behavior. You will train, validate and test a model using Keras. The model will output a steering angle to an autonomous vehicle.
 
 We have provided a simulator where you can steer a car around a track for data collection. You'll use image data and steering angles to train a neural network and then use this model to drive the car autonomously around the track.
 
--- utils.py
+++ utils.py
@@ -20,13 +20,28 @@ Tested on:
 # =============================================================================
 #importing useful packages
 import numpy as np
+import glob
 import cv2
 import csv
 import os
 
 import matplotlib.pyplot as plt
 
+import subprocess
+
 # =============================================================================
+def getClipboardData():
+    p = subprocess.Popen(['xclip','-selection', 'clipboard', '-o'], stdout=subprocess.PIPE)
+    retcode = p.wait()
+    data = p.stdout.read()
+    return data
+
+def setClipboardData(data):
+    p = subprocess.Popen(['xclip','-selection','clipboard'], stdin=subprocess.PIPE)
+    p.stdin.write(data)
+    p.stdin.close()
+    retcode = p.wait()
+
 def print_list_text(img_src, str_list, origin=(0, 0), color=(0, 255, 255), 
     thickness=2, fontScale=0.45,  y_space=20):
 
@@ -67,117 +82,195 @@ def load_dataset(data_path):
     
     """ load dataset from path 
     Args:
-        data_path: `string` path where images files are located
     Returns: 
-        images: `list` of cv2.math of images 
-        measurements: `list` of stering measuments
     """
 
-    lines = []
+    data = []
     with open(data_path) as csvfile:
         reader = csv.reader(csvfile)
         for idx, line in enumerate(reader):
             if idx:
-                lines.append(line)
+                data.append({
+                    "img_c":line[0],
+                    "img_l":line[1],
+                    "img_r":line[2],
+                    "steering": float(line[3]),
+                    })
 
-    folder_path = os.path.dirname(data_path)
+    return data
 
-    images_c = np.asarray([cv2.imread(os.path.join(folder_path, lines[idx][0])) for idx in range(len(lines))])
-    images_l = np.asarray([cv2.imread(os.path.join(folder_path, lines[idx][1])) for idx in range(len(lines))])
-    images_r = np.asarray([cv2.imread(os.path.join(folder_path, lines[idx][2])) for idx in range(len(lines))])
-    measurements = np.asarray([float(lines[idx][3]) for idx in range(len(lines))])
-
-    return images_c, images_l, images_r, measurements
-
-def create_video(images, path, file_name, fps=15., video_size=None):
+def create_video(data, cam_label, dst_path, src_path, file_name, fps=15., 
+    video_size=(320, 160)):
 
     """ Create video from images list 
     Args:
-        images" `list` of cv2.math images 
-        path" `string` path to save videos
+        data" `list` of dictionaries with data
+        cam_label" `string` camera label to record video
+        dst_path" `string` path to save videos
+        src_path" `string` path  where data is stored
         file_name" `string` video file name to save
         fps" `float` desired video frame rate
-        video_size" `tuple` desired video size (widht, height)
+        video_size" `tuple` desired video size (width, height)
     Returns: 
     """
 
-    # video size
-    if video_size is None:
-        video_size = (images.shape[0], images.shape[1])
-
     # Define the codec and format and create VideoWriter object
     fourcc = cv2.VideoWriter_fourcc(*"mp4v")  # Codec H264, format MP4
-    video_name = os.path.join(path, file_name) # File name and format
+    video_name = os.path.join(dst_path, file_name) # File name and format
     video_out = cv2.VideoWriter(video_name, fourcc, fps, video_size) # Video recorder variable
 
     # Write frame to video
-    for img in images:
+    for idx in range(len(data)):
+        img = cv2.imread(os.path.join(dst_path, 'IMG', data[idx][cam_label]))
         video_out.write(cv2.resize(img, video_size))
 
     # Release video variable memory
     video_out.release()
 
-def reproduce_dataset(images, fps=15., loop=True, sc_fc=2., up_limit=0, down_limit=0):
+def reproduce_dataset(data, fps=15., loop=True, sc_fc=2., up_limit=0, 
+    down_limit=0, dataset_path="", CORRECTION=0.2):
 
     """ show dataset 
     Args:
-        images" `list` of cv2.math images 
-        fps" `float` desired video frame rate
-        loop" `boolean` enable/disable looping
-        sc_fc" `float` scaling factor to show video
+        images: `list` of cv2.math images 
+        fps: `float` desired video frame rate
+        loop: `boolean` enable/disable looping
+        sc_fc: `float` scaling factor to show video
+        up_limit: `int` superior limit to ignore image
+        down_limit: `int` inferior limit to ignore image
+        dataset_path: `string` path where dataset `data` is stored
+        CORRECTION: `float` angle correction factor
     Returns: 
     """
 
-    down_limit = images.shape[1]-down_limit
-
+    cam_labels = ("img_l", "img_c", "img_r")
     reproduce = True
     while True:
         idx = 0
-        while idx in range(len(images)):
-
+        while idx in range(len(data)):
             if reproduce:
+                imgs = []
+                for label in cam_labels:
+                    img = cv2.imread(os.path.join(dataset_path, 'IMG', data[idx][label]))
+                    if up_limit or down_limit:
+                        # Draw superior limit
+                        img2 = cv2.rectangle(img.copy(),(0,0),(img.shape[1], up_limit),(0,0,0), -1)
+                        img = cv2.line(img,(0,up_limit),(img.shape[1],up_limit),(0,0,255),2)
+                        # Draw inferior limit
+                        img2 = cv2.rectangle(img2,(0,int(img.shape[0]-down_limit)),(img.shape[1], img.shape[0]),(0,0,0), -1)
+                        img = cv2.line(img,(0, int(img.shape[0]-down_limit)),(img.shape[1],int(img.shape[0]-down_limit)),(0,0,255),2)
+                        # Overlay image
+                        img = cv2.addWeighted(src2 = img2, src1 = img, 
+                            alpha = 0.2, beta = 1, gamma = 0)
+                    img = cv2.resize(img, 
+                        (int(img.shape[1]*sc_fc), int(img.shape[0]*sc_fc)))
+                    angle = data[idx]['steering']
+                    if label == cam_labels[0]: angle += CORRECTION
+                    elif label == cam_labels[2]: angle -= CORRECTION
+                    angle = round(angle, 2)
+                    print_list_text(
+                        img, ("Angle: {}[rad]".format(angle), "cam: {}".format(label)), 
+                        origin=(10, img.shape[0] - 35), color=(255, 0, 255), 
+                        thickness=1, fontScale=0.50,  y_space=20)
+                    imgs.append(img)
 
-                img = images[idx]
+                # Concatenate all images
+                img = np.concatenate(imgs, axis = 1)
 
-                if up_limit or down_limit:
-                    img2 = cv2.rectangle(img.copy(),(0,0),(img.shape[1], up_limit),(0,0,0), -1)
-                    img = cv2.line(img,(0,up_limit),(img.shape[1],up_limit),(0,0,255),2)
-                    img2 = cv2.rectangle(img2,(0,down_limit),(img.shape[1],img.shape[0]),(0,0,0), -1)
-                    img = cv2.line(img,(0,down_limit),(img.shape[1],down_limit),(0,0,255),2)
-                    img = cv2.addWeighted(src2 = img2, src1 = img, 
-                        alpha = 0.2, beta = 1, gamma = 0)
-                img = cv2.resize(img, 
-                    (int(images.shape[0]*sc_fc), int(images.shape[1]*sc_fc)))
-
-                str_list = ("A: stop/reproduce", "Q: Quit", "{}%".format(round(idx*100./len(images), 2)))
+                str_list = ("A: stop/reproduce", "Q: Quit", "{}%".format(round(idx*100./len(data), 2)))
                 print_list_text(
                     img, str_list, origin=(10, 15), color=(0, 255, 255), 
                     thickness=1, fontScale=0.50,  y_space=20)
                 cv2.imshow("data_visualization", img)
 
                 # Increment index
-                idx =-1 if loop and idx == len(images) -1 else (idx+1 if reproduce else idx)
+                idx =-1 if loop and idx == len(data) -1 else (idx+1 if reproduce else idx)
             
             user_inpu = cv2.waitKey(int(1000/fps)) & 0xFF
             if user_inpu == ord('q') or  user_inpu == ord('Q'): break
             if user_inpu == ord('a') or  user_inpu == ord('A'): reproduce = not reproduce
 
         if user_inpu == ord('q') or user_inpu == ord('Q'): break
-        if idx >= len(images) -1: break
+        if idx >= len(data) -1: break
     
     # Destroy GUI/User interface window
     cv2.destroyAllWindows()
 
-def plot_data_distribution(y_data, scfc=30):
+def plot_data_distribution(y_data, scfc=30, graph_name="", save_name=None):
+
+    """ plots the dataset steering angle distribution 
+    Args:
+        y_data: `list` of steering angles
+        scfc: `int` graph's bars scaling factor
+        graph_name: `string` graphs name
+        save_name" `string` absolute path to save graph
+    Returns: 
+    """
 
     plt.figure(figsize=(15,5))
     plt.hist(y_data, density=False, bins= [bin/scfc for bin in range(-scfc, scfc, 1)], facecolor='g', alpha=0.75)
     plt.xlabel('Steering Angles')
-    plt.ylabel('Distirbutions')
-    plt.title('Distirbutions of Steering Angles')
+    plt.ylabel('Distributions')
+    plt.title(graph_name)
     plt.grid(True)
+    if save_name is not None:
+        plt.savefig(save_name)
     plt.show()
 
+def plot_training_history(history_object, fig_size=(20,30), save_name=None):
 
+    """ plots the training loss 
+    Args:
+        history_object: `dic` with training history loss for validation and training
+        fig_size: `tuple` graph's size
+        save_name" `string` absolute path to save graph
+    Returns: 
+    """
+
+    plt.figure(figsize=fig_size)
+    ### plot the training and validation loss for each epoch
+    plt.plot(history_object.history['loss'])
+    plt.plot(history_object.history['val_loss'])
+    plt.title('model mean squared error loss')
+    plt.ylabel('mean squared error loss')
+    plt.xlabel('epoch')
+    plt.legend(['training set', 'validation set'], loc='upper right')
+    plt.grid(True)
+    if save_name is not None:
+            plt.savefig(save_name)
+    plt.show()
+
+def create_model_result_video(src_path, dst_path="", save_name="model_results_cam_c.mp4", 
+    fps=30., video_size=(360, 160)):
+    
+    """ creates models results from behavioral driving images 
+    Args:
+        src_path: `string` absolute path where models results images are stored
+        dst_path" `string` destination path to save video
+        save_name" `string` video file name
+        fps" `float` video frame rate
+        video_size" `tuple` desired video size
+    Returns: 
+    """
+
+    # Check for images extensions
+    extensions = ["jpg","gif","png","tga"]
+
+    # Load images in 'path'
+    imgs_path_list = [item for i in [glob.glob(src_path+'/*.%s' % ext) for ext in extensions] for item in i]
+    
+    # Define the codec and format and create VideoWriter object
+    fourcc = cv2.VideoWriter_fourcc(*"mp4v")  # Codec H264, format MP4
+    video_name = os.path.join(dst_path, save_name) # File name and format
+    video_out = cv2.VideoWriter(video_name, fourcc, fps, video_size) # Video recorder variable
+
+    for imgs_path in imgs_path_list:
+        
+        # Read image and write it to video
+        img = cv2.imread(os.path.join(imgs_path))
+        video_out.write(cv2.resize(img, video_size))
+
+    # Release video variable memory
+    video_out.release()
 
 # =============================================================================
